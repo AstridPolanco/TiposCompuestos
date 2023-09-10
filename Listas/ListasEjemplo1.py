db599184e3f3ae215b55b78466a6e14ec3266c5f
AstridPolanco <135398546+AstridPolanco@users.noreply.github.com>
2023-09-09 23:48:02 -0600

    Add files via upload

---
+++ Listas/ListasEjemplo1.py
@@ -0,0 +1,8 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\HP BLUE\AppData\Local\Programs\Python\Python311\ejemplo3.1.py
+Los elementos de la lista son
+[10, 7, 3, 7, 2]
+La suma de todos sus elementos es
+29
---
+++ Listas/ListasNota.py
@@ -0,0 +1,9 @@
+#Definir una lista por asignación que almacene en el primer
+#componente el nombre de un alumno y en las dos siguientes sus notas.
+#imprimir luego el nombre y el promedio de las dos notas.
+lista=["Ana", 7, 9]
+print("Nombre del alumno:")
+print(lista[0])
+promedio=(lista[1]+lista[2])//2
+print("Promedio de sus dos notas:")
+print(promedio)
